a9c8d4aea2809e808a6fffad2cb92adf47df701e
Orin Fletcher <orin.fletcher@gmail.com>
2021-01-30 14:30:15 -0800

    adds heading state to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // ! useState() can take an object
+  const [fullName, setFullName] = useState({ fName: "", lName: "" });
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const name = event.target.name;
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue,
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
